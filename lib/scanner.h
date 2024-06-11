a240ee41ce85ba21caae04708b2b68a0c7f8bc83
rauudy <raudyc@gmail.com>
2024-06-10 21:14:58 -0600

    Mkdisk

---
+++ lib/scanner.h
@@ -0,0 +1,29 @@
+// Las clases h sirven para definir las funciones y  caracteristicas publicas que vamos a utilizar en mi clase, por eso esta en la carpeta de librerias
+
+#ifndef SCANNER_H
+#define SCANNER_H
+#include <vector>
+#include <string>
+// #include "../lib/disco.h"
+
+using namespace std;
+
+class scanner
+{
+public:
+    scanner();
+    void start();
+    void functions(string token, vector<string> tks); //Recibe el comando y lista de instrucciones
+    string token(string text);                      
+    vector<string> split(string text, string text_split);
+    vector<string> split_tokens(string text);
+    bool compare(string a, string b);
+    string upper(string a);
+    void errores(string operacion, string mensaje);
+    void respuesta(string operacion, string mensaje);
+    void funcion_excec(vector<string> tokens);
+    void excec(string path);
+    bool confirmar(string mensaje);
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include "../lib/scanner.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    scanner scan;
+    scan.start();
 }
